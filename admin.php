41e4cc153d417e12fcf0cddbac294afa1d56f852
William <wescobar@xolit.net>
2023-06-01 20:37:42 +0200

    ajuste del form

--- admin.php
+++ admin.php
@@ -8233,6 +8233,48 @@ if ($ADD=="1A")
 			$voi_count = "$row[0]";
 			}
 		##### END ID override optional section #####
+		echo '<section class="content">';
+			echo '<div class="container-fluid">';
+				echo '<div class="row">';
+					echo '<div class="col-12 text-center">';
+						echo '<h2>Resumen del sistema</h2>';
+					echo '</div>';
+				echo '</div>';
+				echo '<div class="row">';
+					echo '<div class="col-12">';
+
+              echo '<form>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+              </form>';
+					echo '</div>';
+				echo '</div>';
+			echo '</div>';
+		echo '</section>';
 
 		echo "<TABLE><TR><TD>\n";
 		echo "<img src=\"images/icon_black_users.png\" alt=\"Users\" width=42 height=42> <FONT FACE=\"ARIAL,HELVETICA\" COLOR=BLACK SIZE=2>";
