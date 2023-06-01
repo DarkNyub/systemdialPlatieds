d50e1834b2fd9f240413a545544372a7a7026769
William <wescobar@xolit.net>
2023-06-01 21:54:22 +0200

    ajuste del form v2

--- admin.php
+++ admin.php
@@ -8242,35 +8242,19 @@ if ($ADD=="1A")
 				echo '</div>';
 				echo '<div class="row">';
 					echo '<div class="col-12">';
-
-              echo '<form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                  </div>
-                </div>
-              </form>';
+						echo '<form>
+						
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<button type="button" class="btn btn-danger">Action</button>
+									</div>
+									<input type="text" class="form-control">
+								</div>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Email address</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+								</div>
+							</form>';
 					echo '</div>';
 				echo '</div>';
 			echo '</div>';
